2430b183de53fd19a36e49bc1e3e8129f9011b72
ahmed-ali-9-6 <ahmed.ali.hassan108@gmail.com>
2024-08-18 01:52:16 +0300

    observe feature

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-const sectionElements = document.querySelectorAll("section");
-const projectElement = document.querySelector(".projects");
-const observeElements = (sectionElements) => {
+const targetElements = document.querySelectorAll(".target");
+const observeElements = (targetElements) => {
   const options = {threshold: 0.3};
 
   const callback = (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("in-view");
-        projectElement.classList.remove("in-view");
       } else {
         entry.target.classList.remove("in-view");
-        projectElement.classList.add("in-view");
       }
     });
   };
   const observer = new IntersectionObserver(callback, options);
   
-  sectionElements.forEach((element) => observer.observe(element));
+  targetElements.forEach((element) => observer.observe(element));
 };
 
-observeElements(sectionElements);
+observeElements(targetElements);
 
 const footerYear = document.querySelector(".footer-year");
 const currentYear = new Date().getFullYear();
